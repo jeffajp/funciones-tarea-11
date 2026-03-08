4e34f4d1bdcba25d2c8c4507e854a589d08c62aa
HP <HP@DESKTOP-6S1BT0A>
2026-03-08 13:01:07 -0600

    eje4

---
+++ tareaa4/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		// Imprimir una línea separadora utilizando una función Linea() que muestre “----------------”.
+		void Linea()
+		{
+
+	    	Console.WriteLine("-");
+			
+
+		}
+		Linea();
+	}
+}
