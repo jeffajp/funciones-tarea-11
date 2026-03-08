157f3d5b967348ad0983d719050cb664d59ae5a1
HP <HP@DESKTOP-6S1BT0A>
2026-03-08 12:21:47 -0600

    Agregar archivos de proyecto.

---
+++ funciones tarea 11/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		// Mostrar un saludo en pantalla utilizando una función Saludar() que imprima “Hola, bienvenido”.
+			void Saludar()
+			{
+
+			Console.WriteLine("ingrese un nombre: ");
+			string nombre= Console.ReadLine();
+            Console.WriteLine("hola, bienvenido  " + nombre);
+			
+			}
+		Saludar();
+
+
+
+
+	}
+}
