ca065899f249f203b77c3148ac5bbdc6070a87fd
HP <HP@DESKTOP-6S1BT0A>
2026-03-08 13:21:25 -0600

    menu

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Mostrar un menú simple utilizando una función MostrarMenu() con tres opciones.
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		static void Menu()
+		{
+			Console.WriteLine("1. nombre: ");
+			Console.WriteLine("2. grado: ");
+			Console.WriteLine("3. salir: ");
+
+		}
+
+		Menu();
+	}
+}
