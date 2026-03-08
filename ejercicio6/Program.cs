e0d3303172f84c1d303b320033e42dbddf4a1667
HP <HP@DESKTOP-6S1BT0A>
2026-03-08 13:32:12 -0600

    eje6

---
+++ ejercicio6/Program.cs
@@ -0,0 +1,7 @@
+﻿//Saludar a una persona utilizando una función Saludar(string nombre) que imprima “Hola + nombre”.
+
+void Saludar(string nombre)
+{
+	Console.WriteLine("Hola " + nombre);
+}
+Saludar("Anthony Jefferson");
