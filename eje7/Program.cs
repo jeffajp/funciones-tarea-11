2f8a186848bdf0d5a1afda8fff3918dac4a966a8
HP <HP@DESKTOP-6S1BT0A>
2026-03-08 23:02:54 -0600

    eje7

---
+++ eje7/Program.cs
@@ -0,0 +1,10 @@
+﻿//7. Mostrar el doble de un número utilizando una función MostrarDoble(int n).
+void MostrarDoble(int n)
+{
+	int resultado = n * 2;
+	Console.WriteLine(resultado);
+}
+
+Console.WriteLine("ingrese un numero: ");
+int numero= int.Parse(Console.ReadLine());
+MostrarDoble(numero);
