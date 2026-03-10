acca286870089bdb823337684b5224468fc683dd
HP <HP@DESKTOP-6S1BT0A>
2026-03-10 00:05:36 -0600

    eje12

---
+++ eje12/Program.cs
@@ -0,0 +1,13 @@
+﻿//12.Restar dos números utilizando una función Restar(int a, int b) que retorne el resultado.
+
+int Restar(int a, int b)
+{
+	return a - b;
+}
+Console.WriteLine("ingrese un numero: ");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese el segundo numero: ");
+int b= int.Parse(Console.ReadLine());
+
+int resta = Restar(a, b);
+Console.WriteLine(resta);
