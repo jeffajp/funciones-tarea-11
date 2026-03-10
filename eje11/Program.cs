90d1e48b52a42885522e024a1cf5fdc3769d2fe3
HP <HP@DESKTOP-6S1BT0A>
2026-03-09 23:59:30 -0600

    eje11

---
+++ eje11/Program.cs
@@ -0,0 +1,14 @@
+﻿//11.Sumar dos números utilizando una función Sumar(int a, int b) que retorne el resultado.
+
+int Sumar(int a, int b)
+{
+	return a + b;
+
+}
+Console.WriteLine("ingrese el primer operando: ");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese segundo operando: ");
+int b= int.Parse(Console.ReadLine());
+
+int rsultado= Sumar(a, b);
+Console.WriteLine(rsultado);
