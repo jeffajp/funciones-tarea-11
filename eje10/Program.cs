dd6c70288b466c03e1c258831d4c62c85ba9861e
HP <HP@DESKTOP-6S1BT0A>
2026-03-09 23:51:44 -0600

    eje10

---
+++ eje10/Program.cs
@@ -0,0 +1,17 @@
+﻿//10. Imprimir un texto varias veces utilizando una función ImprimirRepetido(string texto, int veces).
+
+void ImprimirRepetido(string texto, int veces)
+{
+	for (int i = 1; i < veces; i++)
+	{
+		Console.WriteLine(texto);
+	}
+	
+}
+Console.WriteLine("ingrese una frase: ");
+string texto= Console.ReadLine();
+Console.WriteLine("ingrese las veces que se repita: ");
+int veces = int.Parse(Console.ReadLine());
+Console.WriteLine(texto);
+
+ImprimirRepetido(texto, veces);
--- eje8/Program.cs
+++ eje8/Program.cs
@@ -1,11 +1,12 @@
 ﻿//8. Mostrar el cuadrado de un número utilizando una función MostrarCuadrado(int n).
 void MostrarCuadrado(int n)
 {
-	Console.WriteLine("ingrese un numero: ");
-	int numero = int.Parse(Console.ReadLine());
-	int resultado = numero * numero;
+	
+	int resultado = n * n;
 	Console.WriteLine(resultado);
 
 }
+Console.WriteLine("ingrese un numero: ");
+int n= int.Parse(Console.ReadLine());
 
-MostrarCuadrado();
+MostrarCuadrado(n);
