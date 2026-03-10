46ac514d99e5ea708c22c9ae09e23a647741cbf8
HP <HP@DESKTOP-6S1BT0A>
2026-03-09 23:22:58 -0600

    eje9

--- eje8/Program.cs
+++ eje8/Program.cs
@@ -1,10 +1,11 @@
 ﻿//8. Mostrar el cuadrado de un número utilizando una función MostrarCuadrado(int n).
 void MostrarCuadrado(int n)
 {
-	int resultado = n * n;
+	Console.WriteLine("ingrese un numero: ");
+	int numero = int.Parse(Console.ReadLine());
+	int resultado = numero * numero;
 	Console.WriteLine(resultado);
 
 }
-Console.WriteLine("ingrese un numero: ");
-int numero= int.Parse(Console.ReadLine());
-MostrarCuadrado(numero);
+
+MostrarCuadrado();
---
+++ eje9/Program.cs
@@ -0,0 +1,13 @@
+﻿//9. Sumar dos números utilizando una función SumarMostrar(int a, int b) y mostrar el resultado.
+int SumarMostrar(int a, int b)
+{
+	return  a + b;
+
+}
+Console.WriteLine("ingrese un digito");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese otro digito");
+int b=int.Parse(Console.ReadLine());
+
+int r =SumarMostrar(a, b);
+Console.WriteLine(r);
