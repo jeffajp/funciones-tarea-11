2093bf9f8cda344e4d92827d2b89261b2db844cb
German <germanjosearielescobarescobar@gmai.com>
2026-03-10 18:15:17 -0600

    eje14

---
+++ eje14/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+double Dividir(double a, double b)
+{
+    return a * b;
+
+}
+Console.WriteLine("ingrese un numero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese segundo numero: ");
+double b = double.Parse(Console.ReadLine());
+
+double resultado = Dividir(a, b);
+Console.WriteLine(resultado);
